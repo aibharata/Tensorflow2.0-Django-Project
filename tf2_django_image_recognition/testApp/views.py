2ee10b5c02a4c3e422e56b1540e45c4487ca0bdd
aibharata <aibharata1@gmail.com>
2021-01-22 18:16:02 +0530

    Update views.py

--- tf2_django_image_recognition/testApp/views.py
+++ tf2_django_image_recognition/testApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import tensorflow as tf
 import numpy as np
+from PIL import Image
+
 from tensorflow.keras.models import load_model, model_from_json
 from tensorflow.keras.preprocessing import image
 import tensorflow.keras.applications.resnet50 as tfApps
@@ -10,23 +12,33 @@ def load_my_model():
 	model.summary()	
 	return model
 
+
+
+
 # This model will (and should) be only loaded once when server starts
 model = load_my_model()
 
 def run_predict(myfile):
-	img = image.load_img(myfile, target_size=(224, 224))
-	img = image.img_to_array(img)
-	img = np.expand_dims(img, axis=0)
-	preds = model.predict(img)
-	result = tfApps.decode_predictions(preds, top=3)[0]
-	return result
+	try:
+		img = Image.open(myfile.file)
+		img = img.convert('RGB')
+		img = img.resize((224, 224), Image.NEAREST)
+		
+		img = image.img_to_array(img)
+		img = np.expand_dims(img, axis=0)
+		preds = model.predict(img)
+		result = tfApps.decode_predictions(preds, top=3)[0]
+		return result
+	except Exception as err:
+		return err
+	
 
 def predict(request):
 	if request.method == 'POST' and request.FILES['img']:
 		try:
+			print(request.FILES['img'])
 			post = request.method == 'POST'
-			myfile = request.FILES['img']
-			result = run_predict(myfile)
+			result = run_predict(request.FILES['img'])
 			#result = "Vin"
 			return render(request, "testApp/predict.html", {
 				'result': result})
